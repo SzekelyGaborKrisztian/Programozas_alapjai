a76e771982689c1bfaecf9bd721a6d5518fd1396
SzekelyGaborKrisztian <szekelygaborkrisztian@gmail.com>
2022-10-19 18:01:37 +0200

    Ez lesz a legjobb projekt a világon.

--- helloworld.c
+++ helloworld.c
@@ -1,10 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
-int main() {
-
-printf("Hello World");
-
-return 0;
+int main(int argc, char const *argv[])
+{
+    printf("Hello world!");
+    return 0;
 }
